0f7148ed7aae142193e5f27a16d93bb6b52f34f5
GilangAqshal <gilangais16@gmail.com>
2026-04-01 14:22:42 +0700

    Updates Tampilan Login Admin & Controller,Views,Routes,Assets by Public

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,6 +5,10 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->get('/home/coba-parameter/(:alpha)/(:num)/(:alphanum)/','Home::belajar_segment/$1/$2/$3');
+$routes->get('/', 'Admin::login');
+// $routes->get('/home/coba-parameter/(:alpha)/(:num)/(:alphanum)/','Home::belajar_segment/$1/$2/$3');
 // $routes->method('link_samaran', 'Controller::Function');
+
+// Rourtes login admin
+;
+$routes -> get('/admin/login-admin','Admin::login');
---
+++ app/Controllers/Admin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+class Admin extends BaseController
+{
+    public function login()
+    {
+        return view('Backend/Template/login');
+    }
+
+
+}
